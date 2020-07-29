e03149cd2183588e5f9ef12e91d28ecafb8aaa94
vivian76 <thiagocabral477@gmail.com>
2020-07-29 11:49:55 -0300

    imprimindo codigo border-radius

--- main.js
+++ main.js
@@ -3,23 +3,35 @@ let topRight = document.querySelector('input[name=top-right]');
 let bottomLeft = document.querySelector('input[name=bottom-left]');
 let bottomRight = document.querySelector('input[name=bottom-right]');
 let box = document.querySelector('.box-border');
+let valueBox = document.querySelector('section .value-borda');
 
 topLeft.onchange = function() {
   let value = `${topLeft.value}px`;
   box.style.borderTopLeftRadius = value; 
+  borderRadius();
 }
 
 topRight.onchange = function() {
   let value = `${topRight.value}px`;
   box.style.borderTopRightRadius = value; 
+  borderRadius();
 }
 
 bottomLeft.onchange = function() {
   let value = `${bottomLeft.value}px`;
   box.style.borderBottomLeftRadius = value; 
+  borderRadius();
 }
 
 bottomRight.onchange = function() {
   let value = `${bottomRight.value}px`;
   box.style.borderBottomRightRadius = value; 
+  borderRadius();
+}
+
+function borderRadius() {
+  let p = document.createElement('p');
+  let conteudo = document.createTextNode(`border-radius: ${topLeft.value}px ${topRight.value}px ${bottomRight.value}px ${bottomLeft.value}px;`);
+  p.appendChild(conteudo);
+  valueBox.appendChild(p);
 }
